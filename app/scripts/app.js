f432a329df0802c26af12f4702fbe6a55e07892d
Jose Gonzalez <jose.gonzalez@webfg.com>
2017-01-18 16:52:26 +0100

    - Routing con ui router

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,6 +3,28 @@
     window.angular
         .module('app', [
             'ui.bootstrap',
-            'ngMessages'
-        ]);
+            'ngMessages',
+            'ui.router'
+        ])
+        .config(configure);
+
+    function configure($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('list', {
+                url: '/list',
+                templateUrl: 'templates/list.html'
+            })
+            .state('addWorker', {
+                url: '/addworker',
+                templateUrl: 'templates/new-worker.html'
+            })
+            .state('viewWorker', {
+                url: '/viewworker/:id',
+                templateUrl: 'templates/view-worker.html'
+            });
+
+        $urlRouterProvider.otherwise('/list')
+    }
+
+    configure.$inject = ['$stateProvider', '$urlRouterProvider']
 }());
